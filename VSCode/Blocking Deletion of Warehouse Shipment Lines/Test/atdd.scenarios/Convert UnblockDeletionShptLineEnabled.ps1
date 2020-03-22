9e0c2a16125963ad077428465c9c892a17fec39b
Luc van Vugt <lvanvugt@fluxxus.nl>
2020-03-22 19:24:03 +0100

    First test case. #1

---
+++ VSCode/Blocking Deletion of Warehouse Shipment Lines/Test/atdd.scenarios/Convert UnblockDeletionShptLineEnabled.ps1
@@ -0,0 +1,8 @@
+& "./atdd.scenarios/UnblockDeletionShptLineEnabled.ps1" | `
+    ConvertTo-ALTestCodeunit `
+        -CodeunitID 75650 `
+        -CodeunitName 'Unblock Deletion Enabled FLX' `
+        -InitializeFunction `
+        -GivenFunctionName "Create {0}" `
+        -ThenFunctionName "Verify {0}" `
+        | Out-File '.\src\codeunit\COD75650.UnblockDeletionEnabledFLX.al'
---
+++ VSCode/Blocking Deletion of Warehouse Shipment Lines/Test/atdd.scenarios/UnblockDeletionShptLineEnabled.ps1
@@ -0,0 +1,11 @@
+Feature 'Unblock Deletion of Whse. Shpt. Line enabled'{
+    Scenario 1 'Delete by user with no allowance manually created whse shpt. line'{
+        Given	'Enable "Unblock Deletion of Shpt. Line" on warehouse setup'
+        Given	'Location with require shipment'
+        Given	'Warehouse employee for current user with no allowance'
+        Given	'Manualy created warehouse shipment from released sales order with one line with require shipment location'
+        When	'Delete warehouse shipment line'
+        Then	'Warehouse shipment line is deleted'
+
+    }
+}
