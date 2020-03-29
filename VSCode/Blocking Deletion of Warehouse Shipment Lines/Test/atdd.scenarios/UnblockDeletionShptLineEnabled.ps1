ff3ab1271dcd2c2bfa7786776103c00897f1433a
Luc van Vugt <lvanvugt@fluxxus.nl>
2020-03-29 12:43:23 +0200

    Corrected typo Manualy to Manually. #1

--- VSCode/Blocking Deletion of Warehouse Shipment Lines/Test/atdd.scenarios/UnblockDeletionShptLineEnabled.ps1
+++ VSCode/Blocking Deletion of Warehouse Shipment Lines/Test/atdd.scenarios/UnblockDeletionShptLineEnabled.ps1
@@ -3,7 +3,7 @@ Feature 'Unblock Deletion of Whse. Shpt. Line enabled'{
         Given	'Enable "Unblock Deletion of Shpt. Line" on warehouse setup'
         Given	'Location with require shipment'
         Given	'Warehouse employee for current user with no allowance'
-        Given	'Manualy created warehouse shipment from released sales order with one line with require shipment location'
+        Given	'Manually created warehouse shipment from released sales order with one line with require shipment location'
         When	'Delete warehouse shipment line'
         Then	'Warehouse shipment line is deleted'
     }
