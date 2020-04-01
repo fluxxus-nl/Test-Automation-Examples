f402688956bd9817956c17b2eaef4838ab00db5c
Luc van Vugt <lvanvugt@fluxxus.nl>
2020-04-01 15:17:22 +0200

    Added ATDD PowerShell definition files. #1

---
+++ VSCode/Blocking Deletion of Warehouse Shipment Lines/Test/atdd.scenarios/Convert UnblockDeletionShptLineDisabled.ps1
@@ -0,0 +1,8 @@
+& "./atdd.scenarios/UnblockDeletionShptLineEnabled.ps1" | `
+    ConvertTo-ALTestCodeunit `
+        -CodeunitID 75651 `
+        -CodeunitName 'Unblock Deletion Disabled FLX' `
+        -InitializeFunction `
+        -GivenFunctionName "Create {0}" `
+        -ThenFunctionName "Verify {0}" `
+        | Out-File '.\src\codeunit\COD75651.UnblockDeletionDisabledFLX_2.al'
